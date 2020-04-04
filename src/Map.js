073ebe4e9ff5eea7272e676f0ca4a75e52f35eaf
Sean Spearman <sean.spearman@procore.com>
2020-04-04 13:29:51 -0700

    Initial commit

---
+++ src/Map.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import ReactMapGL, { Source, Layer}  from "react-map-gl";
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZnJlZWZvcmVzdHNjaG9vbCIsImEiOiJjazhpNWcwcnAwMmxpM2ZwbTFlbmczZmNkIn0.pM5KRLaHYWUeGr0jOo352g'
+
+const ALAMEDA = {"type":"Feature","id":"6001","properties":{"name":"Alameda"},"geometry":{"type":"Polygon","coordinates":[[[-122.269314,37.903958],[-121.557312,37.821804],[-121.469681,37.482234],[-121.475158,37.482234],[-121.853067,37.482234],[-122.083098,37.476757],[-122.11596,37.504141],[-122.31313,37.898481],[-122.269314,37.903958]]]}};
+
+const mapStyle = {
+  version: 8,
+  sources: {
+    points: {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          ALAMEDA
+        ]
+      }
+    },
+  },
+	layers: [
+		{
+			id: 'data',
+			type: 'fill',
+			paint: {
+				'fill-color': {
+					property: 'percentile',
+					stops: [
+						[0, '#3288bd'],
+						[1, '#66c2a5'],
+						[2, '#abdda4'],
+						[3, '#e6f598'],
+						[4, '#ffffbf'],
+						[5, '#fee08b'],
+						[6, '#fdae61'],
+						[7, '#f46d43'],
+						[8, '#d53e4f']
+					]
+				},
+				'fill-opacity': 0.1
+			}
+		}
+	]
+};
+
+function Map() {
+  const [viewport, setViewport] = useState({
+    width: 800,
+    height: 800,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
+  return <ReactMapGL {...viewport} onViewportChange={setViewport} mapboxApiAccessToken={MAPBOX_TOKEN} >
+		<Source type="geojson" data={mapStyle.sources.points.data}>
+			<Layer {...mapStyle.layers[0]} />
+		</Source>
+  </ReactMapGL>
+    ;
+}
+
+export default Map;
